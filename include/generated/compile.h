235dce08e43c329d22c72a35d16b3832996e326d
lujji <tainer.waynes@gmail.com>
2014-12-28 07:30:21 +0300

    clean mrproper

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Wed Aug 20 20:51:15 CEST 2014"
-#define LINUX_COMPILE_BY "Skelton"
-#define LINUX_COMPILE_HOST "Legacy"
-#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
